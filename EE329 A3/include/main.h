adec48569cbdc2b606e3a5a4fba7634402e7a1d9
Ethan Buttram <ethanbuttram02@gmail.com>
2024-04-26 23:10:21 -0700

    working

--- EE329 A3/include/main.h
+++ EE329 A3/include/main.h
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file           : main.h
- * @brief          : Header for main.c file.
- *                   This file contains the common defines of the application.
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2024 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -23,35 +23,37 @@
 #define __MAIN_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l4xx_hal.h"
 
-  /* Private includes ----------------------------------------------------------*/
-  /* USER CODE BEGIN Includes */
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#define STATE_WAIT 0
+#define STATE_ENTER 1
+#define STATE_COUNT 2
+#define STATE_END 3
+/* USER CODE END Includes */
 
-  /* USER CODE END Includes */
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
 
-  /* Exported types ------------------------------------------------------------*/
-  /* USER CODE BEGIN ET */
+/* USER CODE END ET */
 
-  /* USER CODE END ET */
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
 
-  /* Exported constants --------------------------------------------------------*/
-  /* USER CODE BEGIN EC */
+/* USER CODE END EC */
 
-  /* USER CODE END EC */
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
 
-  /* Exported macro ------------------------------------------------------------*/
-  /* USER CODE BEGIN EM */
+/* USER CODE END EM */
 
-  /* USER CODE END EM */
-
-  /* Exported functions prototypes ---------------------------------------------*/
-  void Error_Handler(void);
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
@@ -89,9 +91,9 @@ extern "C"
 #define LD2_Pin GPIO_PIN_7
 #define LD2_GPIO_Port GPIOB
 
-  /* USER CODE BEGIN Private defines */
+/* USER CODE BEGIN Private defines */
 
-  /* USER CODE END Private defines */
+/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
--- EE329 A3/src/main.c
+++ EE329 A3/src/main.c
@@ -4,9 +4,8 @@
  * @file           : main.c
  * @brief          : Main program body
  * PROJECT         : Parallel LCD
- * Author(s)       : Seth Saxena & Ethan
- * 				  stsaxena@calpoly.edu  ssrini22@calpoly.edu
- * Date			: 2023/04/15
+ * Author(s)       : Seth Saxena & Ethan Buttram
+ * Date			: 2023/04/19
  * Compiler		: STM32CubeIDE v.1.15.0
  * Target			: Nucleo-L496ZG
  * Clock			: 4 MGHz MSI to AHB2
@@ -33,76 +32,198 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define CLEAR_SCREEN 0x80
-#define RETURN_HOME 0x40
-#define WRITE_2ND_LINE 0XC0
-
 void SystemClock_Config(void);
+int Countdown(int count);
+int Key_Map(int key_val);
 
 int main(void)
 {
-  HAL_Init();
-  SystemClock_Config();
+	HAL_Init();
+	SystemClock_Config();
 
-  // configure LED
-  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN); // enables GPIO clock
-  // clears bits in pin PA0
-  GPIOA->MODER &= ~(GPIO_MODER_MODE3);
-  // sets PA0 to general-purpose output mode
-  GPIOA->MODER |= (GPIO_MODER_MODE3_0);
-  // sets pin to push-pull
-  GPIOA->OTYPER &= ~(GPIO_OTYPER_OT3);
-  // disables PU and PD resistors connected to PA0
-  GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD3);
-  // sets pin speed to high for pin 0
-  GPIOA->OSPEEDR |= ((3 << GPIO_OSPEEDR_OSPEED3_Pos));
-  // preset PA0 to 0
-  GPIOA->BRR = (GPIO_PIN_3);
+	// configure LED
+	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN); // enables GPIO clock
+	// clears bits in pin PA0
+	GPIOA->MODER &= ~(GPIO_MODER_MODE3);
+	// sets PA0 to general-purpose output mode
+	GPIOA->MODER |= (GPIO_MODER_MODE3_0);
+	// sets pin to push-pull
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT3);
+	// disables PU and PD resistors connected to PA0
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD3);
+	// sets pin speed to high for pin 0
+	GPIOA->OSPEEDR |= ((3 << GPIO_OSPEEDR_OSPEED3_Pos));
+	// preset PA0 to 0
+	GPIOA->BRR = (GPIO_PIN_3);
 
-  // initialize keypad
-  Keypad_Config();
-  // Initialize LCD
-  LCD_config();
-  LCD_init();
-  // write to lCD
+	// initialize keypad
+	Keypad_Config();
+	// Initialize LCD
+	LCD_config();
+	LCD_init();
+	int STATE = STATE_WAIT;
+	// write "EE 329 A3 TIMER "
+	char str[] = "EE 329 A3 TIMER ";
+	char str2[] = "*=SET #=GO 00:00";
+	LCD_write_string(str);
+	LCD_command(0xC0);
+	LCD_write_string(str2);
+	int count = 0; // seconds needed to count
+	char timec[5] = "00:00";
+	int time1[5] = {0, 0, 10, 0, 0};
+	int key_map[17] = {1, 2, 3, 10, 4, 5, 6, 11, 7, 8, 9, 12, -6, 0, -13, 14, 0};
+	int key_idx = 0;
+	while (1)
+	{
+		switch (STATE)
+		{
+		case STATE_WAIT:
+			while (!(Keypad_IsAnyKeyPressed()))
+			{
+				;
+			}
+			key_idx = Keypad_WhichKeyIsPressed();
+			if (key_idx == 12)
+			{
+				STATE = STATE_ENTER;
+			}
+			else if (key_idx == 14)
+			{
+				STATE = STATE_COUNT;
+			}
+			break;
+		case STATE_ENTER:
+			LCD_command(0xCB);
+			LCD_write_string(timec);
+			LCD_command(0xCB);
+			int overflow = 0;
+			for (int num_press = 0; num_press < 5; num_press++)
+			{
+				if (num_press != 2)
+				{
+					if (overflow == 0)
+					{
+						while (!(Keypad_IsAnyKeyPressed()))
+						{
+							delay_us(100);
+						}
+					}
+					key_idx = Keypad_WhichKeyIsPressed();
+					// prevent entries greater than 59:59
+					if (key_map[key_idx] >= 0 && key_map[key_idx] < 10)
+					{
+						time1[num_press] = key_map[key_idx];
+						if ((num_press == 0 || num_press == 3) && (time1[num_press] > 5))
+						{
+							time1[num_press] = 5;
+							overflow = 1;
+							GPIOA->BSRR = GPIO_PIN_3;
+						}
+						else if ((num_press == 1 || num_press == 4) && (overflow == 1))
+						{
+							time1[num_press] = 9;
+							overflow = 0;
+							GPIOA->BRR = GPIO_PIN_3;
+						}
+						count = ((time1[0]) * 60 * 10) + ((time1[1]) * 60) +
+								((time1[3]) * 10) + (time1[4]);
+						LCD_command(0xCB + num_press);
+						LCD_write_char(time1[num_press] + 48);
+					}
+					else if (key_idx == 13)
+					{
+						time1[num_press] = key_map[key_idx];
+						count = ((time1[0]) * 60 * 10) + ((time1[1]) * 60) +
+								((time1[3]) * 10) + (time1[4]);
+						LCD_command(0xCB + num_press);
+						LCD_write_char(time1[num_press] + 48);
+					}
+					else if (key_idx == 12)
+					{
+						STATE = STATE_ENTER;
+						break;
+					}
+				}
+				else
+				{
+					LCD_command(0xCE);
+				}
+				delay_us(500000);
+			}
+			STATE = STATE_WAIT;
+			break;
+		case STATE_COUNT:
+			int i = Countdown(count);
+			if (i == 0)
+			{
+				STATE = STATE_END;
+			}
+			else
+			{
+				STATE = STATE_ENTER;
+			}
+			break;
+		case STATE_END:
+			GPIOA->BSRR = GPIO_PIN_3;
+			while (!(Keypad_IsAnyKeyPressed()))
+			{
+				delay_us(100);
+			}
+			key_idx = Keypad_WhichKeyIsPressed();
+			delay_us(500000);
+			if (key_idx == 14)
+			{
+				GPIOA->BRR = GPIO_PIN_3;
+				STATE = STATE_WAIT;
+			}
+			else
+				STATE = STATE_WAIT;
+			break;
+		default:
+			STATE = STATE_WAIT;
+		}
+	}
+}
 
-  delay_us(200000); // delay for LCD initialization
-
-  LCD_command(CLEAR_SCREEN); // clear screen
-
-  // LCD start message
-  LCD_write_string("EE 329 A3 Timer");
-
-  LCD_command(WRITE_2ND_LINE);
-  LCD_write_string("*=Set #=Go 00:00");
-
-  while (1)
-  {
-    uint32_t sistick1 = SysTick->VAL;
-    uint32_t sistick2 = SysTick->VAL;
-
-    while(sistick2 < 950000)
-    {
-      switch (Keypad_WhichKeyIsPressed()) {
-      case 12:
-        LCD_command(CLEAR_SCREEN);
-        LCD_write_string("* pressed");
-        delay(500);
-        break;
-      break;
-    break;
-
-      case 14: 
-        LCD_command(CLEAR_SCREEN);
-        LCD_write_string("# pressed");
-        break;
-      break;
-    break;
-      }
-      sistick2 = SysTick->VAL;
-    }
-    delay_us(1000000-(sistick2-sistick1));
-  }
+/*
+ * Countdowm function:
+ * 	->Parameter: int array that stores acii values of each char in MM:SS time
+ * 	-the function converts the time from MM:SS to seconds and then counts down
+ * 	 to zero in real time, while updating the the LCD to dispay the MM:SS time
+ * 	 every time it changes (once every second).
+ * 	-> this function has no return
+ */
+int Countdown(int count)
+{
+	int time[5] = {0, 0, 10, 0, 0}; // initialize to "00:00"
+	int val;
+	uint32_t sistick1; // track start time of each cycle in the loop
+	uint32_t sistick2; // track end time of each cycle in the loop
+	while (count > 0)
+	{
+		sistick1 = (SysTick->VAL) / 4000000; // start time
+		count -= 1;
+		time[0] = ((count / 60)) / 10; // stores 10s place of minutes
+		time[1] = ((count / 60)) % 10; // store 1s place of minutes
+		time[3] = ((count % 60)) / 10; // stores 10s place of seconds
+		time[4] = ((count % 60)) % 10; // stores 1s place of seconds
+		for (int i = 0; i < 5; i++)
+		{
+			if (i != 2)
+			{
+				LCD_command(0xCB + i); // moves cursor to correct spot
+				LCD_write_char(time[i] + 48);
+			}
+		}
+		sistick2 = (SysTick->VAL) / 4000000; // end time
+		// delay to maintain one loop cycle per second
+		delay_us(980000 - (sistick2 - sistick1));
+	}
+	if (count == 0)
+	{
+		GPIOA->BSRR = (GPIO_PIN_3); // count over so turn on LED
+	}
+	return val = count;
 }
 
 /**
@@ -111,41 +232,41 @@ int main(void)
  */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-   */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Configure the main internal regulator output voltage
+	 */
+	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = 0;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+	RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+	RCC_OscInitStruct.MSICalibrationValue = 0;
+	RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /**
@@ -229,13 +350,13 @@ void SystemClock_Config(void)
  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -248,9 +369,9 @@ void Error_Handler(void)
  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
